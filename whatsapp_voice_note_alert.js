3df6b4e6a61d46a814d9afa761e7fcf15f73da43
Ashraf Ali <nerveband@Ashrafs-MacBook-Pro.local>
2023-04-19 16:26:31 -0400

    Fixed the asterisks (used one extra one) for emphasis in the output message. Also, trying to tweak the token parameter to get a better result but let's see...

--- whatsapp_voice_note_alert.js
+++ whatsapp_voice_note_alert.js
@@ -13,7 +13,7 @@ const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
 async function getSummaryAndActionSteps(text) {
-  const prompt = `Please summarize the following message and provide any action steps. For action steps, give them a heading and use the checkmark emoji next to them if there are any:\n\n${text}`;
+    const prompt = `Please summarize the following message and provide any action steps. For action steps, give them a heading and use the white heavy check mark emoji next to them if there are any:\n\n${text}`;
 
   try {
     const response = await axios.post(
@@ -21,7 +21,7 @@ async function getSummaryAndActionSteps(text) {
       {
         model: 'text-davinci-003',
         prompt: prompt,
-        max_tokens: 100,
+        max_tokens: 2000,
         n: 1,
         stop: null,
         temperature: 0.5,
@@ -112,7 +112,7 @@ client.on('message', async (msg) => {
           if (numberId) {
             // Add sender's information and the time the message was sent to the transcription output
             const senderInfo = `Sender: ${msg.sender ? msg.sender.pushname : "Unknown"} (${msg.from})\nTime: ${new Date(msg.timestamp * 1000).toLocaleString()}\n\n`;
-            const fullMessage = `You just got a voice note from:\n${senderInfo}**Summary and Action Steps:**\n${summaryAndActionSteps}\n\n**Full Transcription:**\n${outputText}`;
+            const fullMessage = `You just got a voice note from:\n${senderInfo}*Summary and Action Steps:*\n${summaryAndActionSteps}\n\n*Full Transcription:*\n${outputText}`;
             
             await client.sendMessage(numberId._serialized, fullMessage);
             console.log('Transcription sent.');
